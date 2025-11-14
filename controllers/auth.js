a94a8f9f5ee5929461d89aa9d9074dee935271f3
Elvira Keira Ishimwe <ishelvirakeira@gmail.com>
2025-11-14 15:37:22 -0500

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async(req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
@@ -90,29 +90,37 @@ exports.postSignup = (req, res, next) => {
     password: req.body.password,
   });
 
-  User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
-      if (err) {
-        return next(err);
-      }
+  //const user1=async()=>await 
+  const existingUser = await User.findOne(
+    { $or: [{ email: req.body.email }, { userName: req.body.userName }] });
+    //async (err, existingUser) => {
+     // if (err) {
+     //   return next(err);
+     // }
       if (existingUser) {
         req.flash("errors", {
           msg: "Account with that email address or username already exists.",
         });
         return res.redirect("../signup");
       }
-      user.save((err) => {
-        if (err) {
+      await user.save();
+      req.logIn(user, (err)=>{
+        if(err){
           return next(err);
         }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.redirect("/profile");
-        });
-      });
-    }
-  );
+        res.redirect("/profile");
+      })
+       // (err) => {
+        //if (err) {
+         // return next(err);
+       // }
+        //req.logIn(user, (err) => {
+        //  if (err) {
+        //    return next(err);
+        //  }
+        //  res.redirect("/profile");
+      //  });
+     // });
+    //}
+  //);
 };
