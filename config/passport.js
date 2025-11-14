b3cc76a0074e801e6513b6391d44345cf2a3e8ef
Elvira Keira Ishimwe <ishelvirakeira@gmail.com>
2025-11-14 15:37:22 -0500

    first commit

--- config/passport.js
+++ config/passport.js
@@ -2,13 +2,14 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
 
+//we need the verify callback to be an async function; the User.findOne and user.comparePassword calls(asynchronous op) can be await
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+    new LocalStrategy({ usernameField: "email" }, async(email, password, done) => {
+      
+      (async()=>{
+        const user = await User.findOne({ email: email.toLowerCase() });
+  
         if (!user) {
           return done(null, false, { msg: `Email ${email} not found.` });
         }
@@ -18,24 +19,34 @@ module.exports = function (passport) {
               "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
           });
         }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err);
-          }
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { msg: "Invalid email or password." });
+        //comparePassword wrapper
+        const isMatch = await new Promise((resolve, reject)=>{
+          user.comparePassword(password, (err, match)=>{//(err, isMatch) => {
+          if (err) return reject(err);
+          resolve(match);
+          });
         });
-      });
-    })
+          if(!isMatch){
+            return done(null, false, {msg:"Invalid email or password."})
+          }
+          return done(null, user);
+      
+          //if (isMatch) {
+           // return done(null, user);
+          //}
+          //return done(null, false, { msg: "Invalid email or password." });
+        })//().catch(err=>done(err));
+      })
+    //})
   );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  passport.deserializeUser(async(id, done) => {
+    const user = await User.findById(id);
+    done(null, user)
+    //User.findById(id, (err, user) => done(err, user));
   });
 };
